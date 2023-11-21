c38011eb246e51aa2d207950308200b43a35e6f3
Ernestas <97168527+atcaliber@users.noreply.github.com>
2023-11-21 16:54:48 +0200

    updates on navbar issues

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 
-      toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active');
-      });
+toggleButton.addEventListener('click', (event) => {
+ 
+  event.preventDefault();
+
+  navbarLinks.classList.toggle('active');
+});
+
+
 
 
 
